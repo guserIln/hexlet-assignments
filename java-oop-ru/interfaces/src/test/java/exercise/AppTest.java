2eb676461eb9a37f5b73e070afad8185b9a650ee
@hexlet/cli <support@hexlet.io>
2023-01-16 22:17:35 +0500

    submit java-oop-ru/interfaces

--- java-oop-ru/interfaces/src/test/java/exercise/AppTest.java
+++ java-oop-ru/interfaces/src/test/java/exercise/AppTest.java
@@ -1,7 +1,7 @@
 package exercise;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.Assert.*;
+//import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.List;
