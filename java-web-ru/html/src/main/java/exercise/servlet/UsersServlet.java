67b9be72371f6a83dce0b192298420d7f92e9c87
@hexlet/cli <support@hexlet.io>
2023-02-18 22:21:34 +0400

    submit java-web-ru/html

--- java-web-ru/html/src/main/java/exercise/servlet/UsersServlet.java
+++ java-web-ru/html/src/main/java/exercise/servlet/UsersServlet.java
@@ -104,7 +104,51 @@ public class UsersServlet extends HttpServlet {
                  throws IOException {
 
         // BEGIN
-   
+        List<Map> users = getUsers();
+
+        Map<String, String> user = users
+            .stream()
+            .filter(u -> u.get("id").equals(id))
+            .findAny()
+            .orElse(null);
+
+        if (user == null) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        StringBuilder body = new StringBuilder();
+        body.append("""
+            <!DOCTYPE html>
+            <html lang=\"ru\">
+                <head>
+                    <meta charset=\"UTF-8\">
+                    <title>Example application | User</title>
+                    <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css\"
+                          rel=\"stylesheet\"
+                          integrity=\"sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We\"
+                          crossorigin=\"anonymous\">
+                </head>
+                <body>
+                    <div class=\"container\">
+                        <a href=\"/users\">Пользователи</a>
+            """);
+
+        for (Map.Entry<String, String> entry : user.entrySet()) {
+            body.append("<div>");
+            body.append(entry.getKey() + ": " + entry.getValue());
+            body.append("</div>");
+        }
+
+        body.append("""
+                    </div>
+                </body>
+            </html>
+            """);
+
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        out.println(body.toString());
         // END
     }
 }
